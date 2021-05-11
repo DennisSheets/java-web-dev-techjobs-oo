78491e32464108a8d2844a9dd6e3ccc6d62285df
madsur <dennissheets@outlook.com>
2021-05-11 18:24:16 -0500

    because I was told to again

--- src/org/launchcode/techjobs_oo/Tests/JobTesting.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTesting.java
@@ -1,9 +1,24 @@
-package org.launchcode.techjobs_oo.Test;
+package org.launchcode.techjobs_oo.Tests;
 
+import org.junit.Test;
+import org.junit.Before;
 import org.launchcode.techjobs_oo.Job;
-import j
 
-public class Test {
+import static org.junit.Assert.*;
+
+public class JobTesting {
+
+
+    Job jobA = new Job();
+    Job jobB = new Job();
+
+//    @Test
+//    public void TestSettingId() {
+//        assertTrue(jobA.)
+//    }
+
+
+
 
 
 }
